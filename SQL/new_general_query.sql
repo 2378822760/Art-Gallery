8b0f6846bf96764c86b01fca807c82587cc142d3
CaoYuxing <90468784+2378822760@users.noreply.github.com>
2022-12-10 19:24:25 +0800

    Update new_general_query.sql

--- SQL/new_general_query.sql
+++ SQL/new_general_query.sql
@@ -1,11 +1,3 @@
-declare @gid varchar(10);
-set @gid = 'HS001';
-declare @eid varchar(10);
-set @eid = 'H001';
-declare @artistid varchar(10);
-set @artistid = 'MMC152';
-declare @cid varchar(10);
-
 -- 查询所有画廊√
 select GID as 画廊号, GNAME as 画廊名, GLOCATION as 地点 from GALLERY;
 
@@ -31,17 +23,36 @@ from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID
 where ARTISTID in (select distinct EID from EXB_ARTIST where EID = @eid);
 
 -- 查询所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,
-ARTYEAR as 创作年份,ARTPRICE as 参考价,ARTSTATUS as 状态,ARTISTID as 作家号,GID as 所属画廊,EID as 所属展览
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作家号,GID as 所属画廊,EID as 所属展览
+from ARTWORK;
+
+-- 查询某个画廊所有作品√
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
 from ARTWORK
-order by ARTISTID ASC;
+where GID = @gid
 
--- 查询某个画廊所有作品
+-- 查询某个展览所有作品√
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+from ARTWORK
+where EID = @eid
 
--- 查询某个展览所有作品
+-- 查询某个艺术家所有作品√
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+from ARTWORK
+where ARTISTID = @artistid
 
--- 查询某个艺术家所有作品
+-- 查询某个类别所有作品√
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+from ARTWORK
+where ARTTYPE = @type
 
--- 查询某个类别所有作品
-
--- 精确查找作品(按照作品号或者按照作品名)
+-- 精确查找作品(按照作品号或者按照作品名)√
+select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+from ARTWORK
+where ARTID = @artid or ARTTITLE = @name;
