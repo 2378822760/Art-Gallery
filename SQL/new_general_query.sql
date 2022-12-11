95b64e749c331829a678a5044062dcb3f5589244
CaoYuxing <90468784+2378822760@users.noreply.github.com>
2022-12-12 00:29:32 +0800

    Update new_general_query.sql

--- SQL/new_general_query.sql
+++ SQL/new_general_query.sql
@@ -1,58 +1,106 @@
 -- 查询所有画廊√
-select GID as 画廊号, GNAME as 画廊名, GLOCATION as 地点 from GALLERY;
+create proc guest.showAllGallery
+	as
+	select GID as 画廊号, GNAME as 画廊名, GLOCATION as 地点 from GALLERY;
+go
 
 -- 查询所有展览√
-select EXHIBITION.EID as 展览号, ENAME as 展览名, ESTARTDATE as 开始日期, EENDDATE as 结束日期, GNAME as 举办方 
-from EXHIBITION,GALLERY where EXHIBITION.GID = GALLERY.GID;
+-- 使用左外连接，防止有的画廊已经注销
+create proc guest.showAllExb
+	as
+	select EXHIBITION.EID as 展览号, ENAME as 展览名, ESTARTDATE as 开始日期, EENDDATE as 结束日期, GNAME as 举办方 
+	from EXHIBITION left outer join GALLERY on EXHIBITION.GID = GALLERY.GID
+go
 
 -- 查询某个画廊开展的展览√
-select EXHIBITION.EID as 展览号, ENAME as 展览名, ESTARTDATE as 开始日期, EENDDATE as 结束日期, GNAME as 举办方 
-from EXHIBITION,GALLERY where EXHIBITION.GID = GALLERY.GID and EXHIBITION.GID = @gid;
+-- 为了防止画廊注销，可以使用画廊名字或者画廊号查询
+create proc guest.showGalleryExb
+	@gid varchar(10) = NULL
+	-- @gname varchar(20) = NULL
+	as
+	select EXHIBITION.EID as 展览号, ENAME as 展览名, ESTARTDATE as 开始日期, EENDDATE as 结束日期, GNAME as 举办方 
+	from EXHIBITION,GALLERY where EXHIBITION.GID = GALLERY.GID and EXHIBITION.GID = @gid;
+go
 
 -- 查询所有艺术家√
-select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
-from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID
+create proc guest.showAllArtist
+	as
+	select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
+	from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID
+go
 
 -- 查询某个画廊签约的所有艺术家√
-select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
-from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID where ARTIST.GID = @gid;
+create proc guest.showGalleryArtist
+	@gid varchar(10)
+	as
+	select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
+	from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID where ARTIST.GID = @gid;
+go
 
 -- 查询某个展览相关的所有艺术家√
-select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
-from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID 
-where ARTISTID in (select distinct EID from EXB_ARTIST where EID = @eid);
+create proc guest.showExbArtist
+	@eid varchar(10)
+	as
+	select ARTISTID as 艺术家号,ARTISTNAME as 姓名,ARTISTBP as 出生地,ARTISTSTYLE as 作品风格,GNAME as 签约画廊 
+	from ARTIST left outer join GALLERY on ARTIST.GID = GALLERY.GID 
+	where ARTISTID in (select distinct EID from EXB_ARTIST where EID = @eid);
+go
 
 -- 查询所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作家号,GID as 所属画廊,EID as 所属展览
-from ARTWORK;
+create proc guest.showAllArtwk
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作家号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK;
+go
 
--- 查询某个画廊所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
-from ARTWORK
-where GID = @gid
+-- 查询某个画廊所有作品
+create proc guest.showGalleryArtwk
+	@gid varchar(10)
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK
+	where GID = @gid
+go
 
--- 查询某个展览所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
-from ARTWORK
-where EID = @eid
+-- 查询某个展览所有作品
+create proc guest.showExbArtwk
+	@eid varchar(10)
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK
+	where EID = @eid
+go
 
--- 查询某个艺术家所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
-from ARTWORK
-where ARTISTID = @artistid
+-- 查询某个艺术家所有作品
+create proc guest.showArtistArtwk
+	@artistid varchar(10)
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK
+	where ARTISTID = @artistid
+go
 
--- 查询某个类别所有作品√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
-from ARTWORK
-where ARTTYPE = @type
+-- 查询某个类别所有作品
+create proc guest.showTypicalArtwk
+	@type varchar(10)
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK
+	where ARTTYPE = @type
+go
 
--- 精确查找作品(按照作品号或者按照作品名)√
-select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
-ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
-from ARTWORK
-where ARTID = @artid or ARTTITLE = @name;
+-- 精确查找作品(按照作品号或者按照作品名)
+create proc guest.exactFindArtwk
+	@artid varchar(10),
+	@name varchar(20)
+	as
+	select ARTID as 作品号,ARTTITLE as 作品名, ARTTYPE as 作品类型,ARTYEAR as 创作年份,ARTPRICE as 参考价,
+	ARTSTATUS as 状态,ARTISTID as 作者号,GID as 所属画廊,EID as 所属展览
+	from ARTWORK
+	where ARTID = @artid or ARTTITLE = @name;
+go
